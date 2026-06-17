9429eaa7b7563ebc573727332326d1b571c68ff2
br-publications <publications.br.app@gmail.com>
2026-06-18 03:52:34 +0530

    Delete src directory

--- src/config/constants.ts
+++
@@ -1,6 +0,0 @@
-export const APP_NAME = "Nova Store";
-export const APP_DESCRIPTION = "The future of premium commerce.";
-
-export const CONSTANTS = {
-  BACKEND_URL: process.env.BACKEND_URL || "https://fakestoreapi.com",
-};
